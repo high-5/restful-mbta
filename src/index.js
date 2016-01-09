878650e6baa43bfb3e986a608a9c17a08f6a0605
~maxmechanic~ <maxmechanic@users.noreply.github.com>
2016-01-09 14:46:40 -0500

    Add Standard for linting, conform code to it

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import request from 'request'
 
 const API_URL = 'http://realtime.mbta.com/developer/api/v1/'
 
-function parseRoutes({ mode }) {
+function parseRoutes ({ mode }) {
   return mode.reduce((result, routeType) => {
     const {route_type, mode_name} = routeType
     return result.concat(
@@ -13,7 +13,7 @@ function parseRoutes({ mode }) {
 
 const parse = { routes: parseRoutes }
 
-function makeRequest({ type, config }) {
+function makeRequest ({ type, config }) {
   return new Promise((resolve, reject) => {
     request.get(config, (e, r, body) => {
       if (e) reject(e)
@@ -25,15 +25,14 @@ function makeRequest({ type, config }) {
   })
 }
 
-
 export default class MBTA {
-  constructor(api_key, api_url = API_URL) {
+  constructor (api_key, api_url = API_URL) {
     this.api_key = api_key
     this.api_url = api_url
     this._parse = { routes: this._parseRoutes }
   }
 
-  buildOptions(path, opts = {}) {
+  buildOptions (path, opts = {}) {
     const { api_key, api_url } = this
     const qs = { ...opts, api_key }
     const config = {
@@ -45,48 +44,48 @@ export default class MBTA {
     return { config, type: path }
   }
 
-  servertime() {
+  servertime () {
     return makeRequest(this.buildOptions('servertime'))
   }
-  routes() {
+  routes () {
     return makeRequest(this.buildOptions('routes'))
   }
-  routesByStop(stop) {
+  routesByStop (stop) {
     return makeRequest(this.buildOptions('routesbystop', { stop }))
   }
 
-  stopsByRoute(route) {
+  stopsByRoute (route) {
     return makeRequest(this.buildOptions('stopsbyroute', { route }))
   }
 
-  stopsByLocation({lat, lon}) {
+  stopsByLocation ({lat, lon}) {
     return makeRequest(this.buildOptions('stopsbylocation', { lat, lon }))
   }
 
-  scheduleByStop(opts) {
+  scheduleByStop (opts) {
     return makeRequest(this.buildOptions('schedulebystop', opts))
   }
 
-  scheduleByRoute(opts) {
+  scheduleByRoute (opts) {
     return makeRequest(this.buildOptions('schedulebyroute', opts))
   }
 
-  scheduleByTrip(opts) {
+  scheduleByTrip (opts) {
     return makeRequest(this.buildOptions('schedulebytrip', opts))
   }
 
-  alerts() {
+  alerts () {
     return makeRequest(this.buildOptions('alerts'))
   }
-  alertByID(id) {
+  alertByID (id) {
     return makeRequest(this.buildOptions('alertbyid', { id }))
   }
 
-  alertHeadersByRoute(route) {
+  alertHeadersByRoute (route) {
     return makeRequest(this.buildOptions('alertheadersbyroute', { route }))
   }
 
-  alertHeadersByStop(stop) {
+  alertHeadersByStop (stop) {
     return makeRequest(this.buildOptions('alertheadersbystop', { stop }))
   }
 }
